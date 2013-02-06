8a8b506f1a25f4ef8d49ac120f392ed1b1ec1f7a
momohuri <maurin.lenglart@gmail.com>
2013-02-05 19:01:26 -0800

    first IA

--- Checkers/Main.rb
+++ Checkers/Main.rb
@@ -4,7 +4,7 @@ test = Board_Manager.new(Board.new)
 
 test.calculation_board.tab_piece[5][2] = Piece.new(false, false)
 test.calculation_board.tab_piece[0][3] = nil
-test.calculation_board.tab_piece[6][1] = Piece.new(true, false) # mettre en king pour plus de resultats
+test.calculation_board.tab_piece[6][1] = Piece.new(true, true) # mettre en king pour plus de resultats
 test.move(2, 5, 3, 4)
 test.move(2, 1, 3, 0)
 test.move(2, 3, 3, 2)
@@ -19,3 +19,38 @@ test.display(false)
 test.number_of_moves()
 test.play_selected_move(0)
 test.display(false)
+
+
+plateau = Board_Manager.new(Board.new)
+
+
+def next_best_shoot(plateau)
+
+  moves = plateau.search_player_moves()
+
+  print moves[0]!=false
+  max=0
+  coup=0
+  moves.to_enum.with_index(1).each do |elem, i|
+       if max<elem.count-1
+        max= elem.count-1
+        coup= i
+      end    
+    end      
+    
+    if max == 1 
+     random_number= Random.new().rand(0..moves.length)
+    end
+    #plateau.play_selected_move(coup)  
+    return moves[coup]
+  
+end
+
+coup=next_best_shoot(plateau)
+print coup
+
+
+#plateau.display(false)
+
+print "finit"
+ 
